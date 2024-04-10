456f065d8c590832da1b24aded54de24b6a766ec
John <wheeler6123@gmail.com>
2024-04-10 17:31:09 -0600

    finished carousel and adding to hero, created searchform component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "./ui/Navbar";
 import Hero from './ui/Hero';
+import SearchForm from "./ui/components/SearchForm";
 
 export default function Home() {
   return (
     <main>
         <Navbar />
         <Hero />
+        <SearchForm />
     </main>
   );
 }
